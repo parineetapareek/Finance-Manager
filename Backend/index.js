95cb6a05b593d795595fb89873423bb3787318a6
Parineeta  Pareek <parineetapareek215@gmail.com>
2025-02-26 00:28:10 +0530

    Started with the Backend - database connectivity done and important packages installed

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+ 
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(()=>{
+        console.log("Connected to MongoDB");
+    })
+    .catch((err)=>{
+        console.error(err);
+    });
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
