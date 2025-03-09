a1c0a716faa880b2952d083429fd112c8828c1e9
Parineeta  Pareek <parineetapareek215@gmail.com>
2025-03-10 01:33:59 +0530

    minor implementation of budget

---
+++ Backend/routes/budget.route.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  createBudget,
+  deleteBudget,
+  getBudgetById,
+  getBudgets,
+  toggleLockBudget,
+  updateBudget,
+} from "../controllers/budget.controller.js";
+
+const budgetRouter = express.Router();
+
+router.post("/create", createBudget);
+router.get("/userBudget/:userId", getBudgets);
+router.get("/budget/:id", getBudgetById);
+router.put("/update/:id", updateBudget);
+router.delete("/delete/:id", deleteBudget);
+router.put("/lock/:id", toggleLockBudget);
+
+export default budgetRouter;
